fb118c1769d7c6f057d46bcca55eeff6d4c96cd0
Laura Nagy <ngylaura@gmail.com>
2018-08-17 16:09:23 +0100

    adds CRUD to film

---
+++ models/film.rb
@@ -0,0 +1,64 @@
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price
+
+    def initialize(options)
+      @title = options['title']
+      @price = options['price'].to_i
+      @id = options['id'].to_i if options['id']
+    end
+
+    def save()
+      sql = "
+        INSERT INTO films
+        (title, price)
+        VALUES
+        ($1, $2)
+        RETURNING *
+      "
+      values = [@title, @price]
+      @id = SqlRunner.run(sql, values)[0]['id'].to_i
+    end
+
+    def update()
+      sql = "
+        UPDATE
+          films
+        SET
+          (title, price) = ($1, $2)
+        WHERE
+          id = $3
+        "
+      values = [@title, @price, @id]
+      SqlRunner.run(sql, values)
+    end
+
+    def delete()
+      sql = "
+        DELETE FROM
+          films
+        WHERE
+          id = $1
+        "
+      values = [@id]
+      SqlRunner.run(sql, values)
+    end
+
+    def Film.all()
+      sql = "SELECT * FROM films"
+      films = SqlRunner.run(sql)
+      return self.map_items(films)
+    end
+
+    def self.map_items(film_data)
+      result = film_data.map { |film| Film.new(film) }
+      return result
+    end
+
+    def Film.delete_all()
+      sql = "DELETE FROM films"
+      SqlRunner.run(sql)
+    end
+
+end
