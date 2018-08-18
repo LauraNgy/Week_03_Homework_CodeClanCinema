3de88dcb90a1d8cb1a3a52e123bd22bbcff50f72
Laura Nagy <ngylaura@gmail.com>
2018-08-18 08:38:02 +0100

    adds films method to customer class

--- models/customer.rb
+++ models/customer.rb
@@ -45,6 +45,29 @@ class Customer
       SqlRunner.run(sql, values)
     end
 
+    def films()
+      sql = "
+        SELECT films.* FROM
+          films
+        INNER JOIN
+          screenings
+        ON
+          screenings.film_id = films.id
+        INNER JOIN
+          tickets
+        ON
+          tickets.screening_id = screenings.id
+        INNER JOIN
+          customers
+        ON
+          tickets.customer_id = $1
+      "
+      values = [@id]
+      films = SqlRunner.run(sql, values)
+      result = Film.map_items(films)
+      return result
+    end
+
     def Customer.all()
       sql = "SELECT * FROM customers"
       customers = SqlRunner.run(sql)
